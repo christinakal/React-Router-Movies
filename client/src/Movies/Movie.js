0b02dc818285f824e5388a1c2093236cdfe4e132
christina kaliora <develop@pirron.one>
2019-12-17 21:48:50 +0100

    something worked

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from "react-router-dom";
+import MovieCard from "./MovieCard";
 
 const Movie = (props) => {
   const [movie, setMovie] = useState();
+
+  console.log('Movie', movie)
  
   useEffect(() => {
     const id = props.id;
@@ -14,6 +17,7 @@ const Movie = (props) => {
         .get(`http://localhost:5000/api/movies/${id}`)
         .then(response => {
           setMovie(response.data);
+          console.log(response.data);
         })
         .catch(error => {
           console.error(error);
@@ -22,10 +26,10 @@ const Movie = (props) => {
   },[]);
   
   // Uncomment this only when you have moved on to the stretch goals
-  // const saveMovie = () => {
-  //   const addToSavedList = props.addToSavedList;
-  //   addToSavedList(movie)
-  // }
+  const saveMovie = () => {
+    const addToSavedList = props.addToSavedList;
+    addToSavedList(movie)
+  }
 
   if (!movie) {
     return <div>Loading movie information...</div>;
@@ -34,27 +38,7 @@ const Movie = (props) => {
   const { title, director, metascore, stars } = movie;
  
   return (
-    <div className="save-wrapper">
-      <div className="movie-card">
-        <Link to={`/item-list/${movie.id}`}>
-          <h2>{movie.title}</h2>
-          <div className="movie-director">
-            Director: <em>{movie.director}</em>
-          </div>
-          <div className="movie-metascore">
-            Metascore: <strong>{movie.metascore}</strong>
-          </div>
-          <h3>Actors</h3>
-
-          {stars.map(star => (
-            <div key={star} className="movie-star">
-              {star}
-            </div>
-          ))}
-        </Link>
-      </div>
-      <div className="save-button">Save</div>
-    </div>
+    <MovieCard movie={movie}/>
   );
 }
 
--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 const MovieCard = props => {
   return (
-    <div className="movie-card">
+    <div className="save-wrapper">
+      <div className="movie-card">
+        <Link to={`/item-list/${props.id}`}>
+          <h2>{props.title}</h2>
+          <div className="movie-director">
+            Director: <em>{props.director}</em>
+          </div>
+          <div className="movie-metascore">
+            Metascore: <strong>{props.metascore}</strong>
+          </div>
+          <h3>Actors</h3>
 
+          {props.movie.stars.map(star => (
+            <div key={star} className="movie-star">
+              {star}
+            </div>
+          ))}
+        </Link>
+      </div>
+      <div className="save-button">Save</div>
     </div>
   );
 };
