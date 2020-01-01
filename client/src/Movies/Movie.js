6f9ba9b7ffc15efd50b01c8b97d0416af1525568
christina kaliora <develop@pirron.one>
2020-01-02 00:59:03 +0100

    Working on the save feature

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link, useParams } from "react-router-dom";
 import MovieCard from "./MovieCard";
+import './Movie.css'
 
 
 const Movie = (props) => {
@@ -11,6 +12,7 @@ const Movie = (props) => {
  
   useEffect(() => {
 
+    // THE HACKY WAY!
     //let id = window.location.href.split('/')[window.location.href.split('/').length-1];
 
     // change ^^^ that line and grab the id from the URL
@@ -29,10 +31,10 @@ const Movie = (props) => {
   },[]);
   
   // Uncomment this only when you have moved on to the stretch goals
-  // const saveMovie = () => {
-  //   const addToSavedList = props.addToSavedList;
-  //   addToSavedList(movie)
-  // }
+  const saveMovie = () => {
+    const addToSavedList = props.addToSavedList;
+    addToSavedList(movie) 
+  }
 
   if (!movie) {
     return <div>Loading movie information...</div>;
@@ -58,7 +60,7 @@ const Movie = (props) => {
               </div>
             ))}
         </div>
-      <div className="save-button">Save</div>
+      <div className="save-button" onClick={saveMovie}>Save</div>
     </div>
   );
 }
--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -4,25 +4,6 @@ import { Link } from "react-router-dom";
 const MovieCard = props => {
   return (
     <div></div>
-  //   <div className="save-wrapper">
-  //     <div className="movie-card">
-  //         <h2>{props.title}</h2>
-  //         <div className="movie-director">
-  //           Director: <em>{props.director}</em>
-  //         </div>
-  //         <div className="movie-metascore">
-  //           Metascore: <strong>{props.metascore}</strong>
-  //         </div>
-  //         <h3>Actors</h3>
-
-  //         {props.movie.stars.map(star => (
-  //           <div key={star} className="movie-star">
-  //             {star}
-  //           </div>
-  //         ))}
-  //     </div>
-  //     <div className="save-button">Save</div>
-  //   </div>
   );
 };
 
